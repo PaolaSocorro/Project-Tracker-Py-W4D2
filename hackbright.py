910f3886a99e40a8cae0f18e8168328b2a8f4bf3
Hackbright Student <info@hackbrightacademy.com>
2015-07-21 16:21:31 -0700

    all done

--- hackbright.py
+++ hackbright.py
@@ -29,23 +29,50 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    pass
+    QUERY = """
+        INSERT INTO Students VALUES (?, ?, ?)
+        """
+    db_cursor.execute(QUERY,(first_name, last_name, github))
+    db_connection.commit()
+    print "Successfully added student: %s %s" % (first_name, last_name)
+    
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+    QUERY = """
+        SELECT description, max_grade
+        FROM Projects
+        WHERE title = ? 
+        """
+    db_cursor.execute(QUERY,(title,))
+    row = db_cursor.fetchone()
+    print "Description: %s and Grade: %s" % (row[0],row[1])
 
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
-    pass
+    QUERY = """
+        SELECT grade
+        FROM grades
+        WHERE student_github = ? AND project_title = ?
+        """
+    db_cursor.execute(QUERY,(github, title))
+    row = db_cursor.fetchone()
+    print "Grade:%s" % (row)
+
 
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
-    pass
+    
+    QUERY = """
+        INSERT INTO Grades VALUES (?,?,?)
+        """
 
+    db_cursor.execute(QUERY,(github,title,grade))
+    db_connection.commit()
+    print "Successfully added %s\'s %s Project with a grade of %s" % (github,title,grade)
 
 def handle_input():
     """Main loop.
@@ -68,6 +95,16 @@ def handle_input():
         elif command == "new_student":
             first_name, last_name, github = args   # unpack!
             make_new_student(first_name, last_name, github)
+        elif command == "project_title":
+            title = args[0]
+            get_project_by_title(title)
+
+        elif command == "grade":
+            github, title = args
+            get_grade_by_github_title(github, title)
+        elif command == "add_grade":
+            github,title,grade = args
+            assign_grade(github, title, grade)
 
 
 if __name__ == "__main__":
